36e0bf6b1cc550999eb0bfd4deb7694a52ae5909
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-04-08 17:32:36 -0500

    Finished sellability report

--- harvest.py
+++ harvest.py
@@ -145,11 +145,46 @@ class Melon:
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
+    #dictionary of melontypes and their characteristics
+    melons_by_id = make_melon_type_lookup(melon_types)
+   
+    melons = []
 
-    # Fill in the rest
+    #create Melon objects according to given info
+    melon_1 = Melon(melons_by_id["yw"], 8, 7, 2, "Sheila")
+    melon_2 = Melon(melons_by_id["yw"], 3, 4, 2, "Sheila")
+    melon_3 = Melon(melons_by_id["yw"], 9, 8, 3, "Sheila")
+    melon_4 = Melon(melons_by_id["cas"], 10, 6, 35, "Sheila")
+    melon_5 = Melon(melons_by_id["cren"], 8, 9, 35, "Michael")
+    melon_6 = Melon(melons_by_id["cren"], 8, 2, 35, "Michael")
+    melon_7 = Melon(melons_by_id["cren"], 2, 3, 4, "Michael")
+    melon_8 = Melon(melons_by_id["musk"], 6, 7, 4, "Michael")
+    melon_9 = Melon(melons_by_id["yw"], 7, 10, 3, "Michael")
+    #extend all-melons by all melons 
+
+    melons.extend(
+        [
+            melon_1,
+            melon_2,
+            melon_3,
+            melon_4,
+            melon_5,
+            melon_6,
+            melon_7,
+            melon_8,
+            melon_9,
+        ]
+    )
+
+    return melons
 
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
     # Fill in the rest
+    for melon in melons:
+        harvested_by = f"Harvested by {melon.harvested_by}"
+        field_num = f"FIELD #{melon.harvested_from}"
+        status = 'CAN BE SOLD' if melon.is_sellable() else 'NOT SELLABLE'
+        print(f"{harvested_by} from {field_num} {status}")
